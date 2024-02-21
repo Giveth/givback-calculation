8187df5bbb36b7088e835bd6895027f3424aa041
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-02-21 16:19:08 +0330

    Fix and now /calculate-updated works properly

--- src/utils.ts
+++ src/utils.ts
@@ -7,14 +7,16 @@ const {hexlify, solidityKeccak256} = ethers.utils;
 const _ = require('underscore');
 const axios = require('axios');
 
-export const convertMinimalDonationToDonationResponse  = (params :{
-  minimalDonationsArray : MinimalDonation[],
-  niceDonationsWithShare ?: MinimalDonation[],
+export const convertMinimalDonationToDonationResponse = (params: {
+  minimalDonationsArray: MinimalDonation[],
+  niceDonationsWithShare?: MinimalDonation[],
   raisedValueSum: number,
   givPrice: number
 }): DonationResponse[] => {
-  const {minimalDonationsArray, raisedValueSum,
-    givPrice} = params
+  const {
+    minimalDonationsArray, raisedValueSum,
+    givPrice
+  } = params
   return minimalDonationsArray.map((item: MinimalDonation) => {
     const share = item.totalDonationsUsdValueAfterGivFactor / raisedValueSum;
     const givback = (item.totalDonationsUsdValueAfterGivFactor / givPrice)
@@ -76,7 +78,8 @@ export const createSmartContractCallAddBatchParams = async (params: {
   nrGIVAddress: string,
   donationsWithShare: DonationResponse[],
   givRelayerAddress: string,
-  network : 'gnosis' | 'optimism'
+  network: 'gnosis' | 'optimism',
+  startDate: string, endDate: String
 }, maxAddressesPerFunctionCall: number): Promise<{
   result: string,
   hashParams: string
@@ -87,8 +90,7 @@ export const createSmartContractCallAddBatchParams = async (params: {
     network
   } = params;
   if (donationsWithShare.length === 0) {
-    throw new Error('There is no eligible donations in this time range')
-
+    throw new Error(`There is no eligible donations in this time range: ${params.startDate} to ${params.endDate}`)
   }
   const partNumbers = donationsWithShare.length / maxAddressesPerFunctionCall
   const hashParams: any = {
@@ -191,8 +193,8 @@ const xdaiWeb3 = new Web3(xdaiWeb3NodeUrl);
 const optimismWeb3NodeUrl = process.env.OPTIMISM_NODE_HTTP_URL
 const optimismWeb3 = new Web3(optimismWeb3NodeUrl);
 
-export const getLastNonceForWalletAddress = async (walletAddress: string, chain:'gnosis' | 'optimism'): Promise<number> => {
-  const web3Provider = chain === 'optimism' ? optimismWeb3 :xdaiWeb3
+export const getLastNonceForWalletAddress = async (walletAddress: string, chain: 'gnosis' | 'optimism'): Promise<number> => {
+  const web3Provider = chain === 'optimism' ? optimismWeb3 : xdaiWeb3
   const userTransactionsCount = await web3Provider.eth.getTransactionCount(
     walletAddress
   );
@@ -202,7 +204,7 @@ export const getLastNonceForWalletAddress = async (walletAddress: string, chain:
     walletAddress
   })
   // prevent sending negative nonce
-  return Math.max(userTransactionsCount - 1 , 0)
+  return Math.max(userTransactionsCount - 1, 0)
 }
 
 
@@ -251,12 +253,14 @@ export const calculateReferralRewardFromRemainingAmount = (valueUsdAfterDeductio
   return calculateReferralReward(originalValue)
 }
 
-export const getBlockbyTimestamp = async (timestamp: number, chainId: number) => {
-  try{
-    const response = await axios.get(`https://api.findblock.xyz/v1/chain/${chainId}/block/before/${timestamp}?inclusive=true`)
+export const getBlockByTimestamp = async (timestamp: number, chainId: number) :Promise<number>=> {
+  try {
+    const url = `https://api.findblock.xyz/v1/chain/${chainId}/block/before/${timestamp}?inclusive=true`
+    console.log('getBlockByTimestamp url', url)
+    const response = await axios.get(url)
     return response.data.number
-  } catch(e) {
-    console.log('getBlockbyTimestamp error', e)
+  } catch (e) {
+    console.log('getBlockByTimestamp error', e)
     return 0
   }
 }
